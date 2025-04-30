57550bac64b51c7c228529f1641cbcdcfdd18627
Matej Rohac <matej.rohac24@gmail.com>
2025-04-30 19:08:03 +0200

    fix add screen a add viewmodel - prekopane od zakladov

--- app/src/main/java/com/example/pathpulse/screens/memories/AddScreen.kt
+++ app/src/main/java/com/example/pathpulse/screens/memories/AddScreen.kt
@@ -1,22 +1,32 @@
 package com.example.pathpulse.screens.memories
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material.icons.rounded.StarOutline
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.SearchBar
@@ -24,17 +34,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pathpulse.AppViewModelProvider
+import com.example.pathpulse.R
 import com.example.pathpulse.ui.theme.PathPulseTheme
 import kotlinx.coroutines.launch
 
@@ -52,50 +62,60 @@ fun AddScreen(
     if (uiState.searchActive) {
         Box(modifier = modifier.fillMaxSize())
         {
-            AddSearchBar(query = uiState.searchQuery, viewModel = viewModel)
+            AddSearchBar(
+                query = uiState.searchQuery,
+                viewModel = viewModel
+            )
         }
     } else {
         Column(
             modifier = modifier
                 .fillMaxWidth()
                 .verticalScroll(scrollState)
-                .padding(16.dp)
+                .padding(dimensionResource(R.dimen.medium_padding))
         ) {
 
-            Box(modifier = modifier.fillMaxSize())
-            {
-                AddSearchBar(query = uiState.searchQuery, viewModel = viewModel)
-            }
+            AddSearchBar(
+                query = uiState.searchQuery,
+                viewModel = viewModel
+            )
 
-            Spacer(Modifier.height(20.dp))
+            Spacer(Modifier.height(dimensionResource(R.dimen.large_spacer)))
 
-            Text(text = "Describe your memory")
+            Text(
+                text = stringResource(R.string.describe_your_memory),
+                style = MaterialTheme.typography.headlineSmall
+            )
+            
             OutlinedTextField(
-                value = uiState.countryDetails.description.orEmpty(),
+                value = uiState.countryDetails.description,
                 onValueChange = viewModel::onDescriptionChange,
-                label = { Text("My journey to...") },
+                label = {
+                    Text(
+                        text = stringResource(R.string.my_journey_to),
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                },
                 modifier = Modifier
                     .fillMaxWidth()
                     .heightIn(min = 150.dp, max = 250.dp),
                 maxLines = 10,
-                shape = RoundedCornerShape(16.dp),
+                shape = RoundedCornerShape(dimensionResource(R.dimen.rounded_shape_corner)),
             )
 
-            Spacer(Modifier.height(20.dp))
+            Spacer(Modifier.height(dimensionResource(R.dimen.large_spacer)))
 
-            Text(text = "Load one your favourite picture")
-            Button(
-                onClick = {
-                    // TODO: Implementácia načítania obrázku
-                },
-                shape = RoundedCornerShape(3.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Gray),
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text("+")
-            }
+            Text(
+                text = stringResource(R.string.rate_your_journey),
+                style = MaterialTheme.typography.headlineSmall
+            )
 
-            Spacer(Modifier.height(20.dp))
+            RatingBar(
+                rating = uiState.countryDetails.rating,
+                onRatingChanged = viewModel::onRatingChange,
+            )
+
+            Spacer(Modifier.height(dimensionResource(R.dimen.large_spacer)))
 
             Button(
                 enabled = uiState.isEntryValid,
@@ -105,7 +125,7 @@ fun AddScreen(
                         navController.popBackStack()
                     }
                 },
-                shape = RoundedCornerShape(16.dp),
+                shape = RoundedCornerShape(dimensionResource(R.dimen.rounded_shape_corner)),
 
                 colors = ButtonDefaults.buttonColors(
                     containerColor = MaterialTheme.colorScheme.primary
@@ -114,13 +134,19 @@ fun AddScreen(
                     .fillMaxWidth()
                     .heightIn(min = 100.dp, max = 200.dp)
             ) {
-                Text("ADD IN COLLECTION")
+                Text(
+                    text = stringResource(R.string.add_in_collection),
+                    style = MaterialTheme.typography.headlineMedium
+                )
             }
-
         }
     }
 }
 
+/**
+ *  search bar tuto = https://www.youtube.com/watch?v=fKtCP9TcPaw
+ *
+ */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddSearchBar(
@@ -134,16 +160,44 @@ fun AddSearchBar(
         onQueryChange = { newQuery ->
             viewModel.onSearchQueryChange(newQuery)
         },
-        onSearch = {
-            viewModel.onSearchQuerySubmit()
-        },
+        onSearch = {},
         active = uiState.searchActive,
         onActiveChange = { isActive ->
             viewModel.onSearchBarActiveChange(isActive)
         },
-        placeholder = { Text("Search country") },
+        placeholder = {
+            Text(
+                text = stringResource(R.string.find_your_country),
+                style = MaterialTheme.typography.bodyLarge
+            )
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Filled.Search,
+                contentDescription = null
+            )
+        },
+        trailingIcon = {
+            if (uiState.searchActive) {
+                IconButton(onClick = {
+                    if (uiState.searchQuery.isNotEmpty()) {
+                        viewModel.onSearchQueryChange("")
+                    } else {
+                        viewModel.onSearchBarActiveChange(false)
+                    }
+                }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = null
+                    )
+                }
+            }
+        },
         modifier = Modifier
+            .clip(RoundedCornerShape(dimensionResource(R.dimen.rounded_shape_corner)))
             .fillMaxWidth()
+
     ) {
         LazyColumn() {
             items(uiState.searchResults) { country ->
@@ -156,9 +210,41 @@ fun AddSearchBar(
                             viewModel.onCountrySelected(country)
                             viewModel.onSearchBarActiveChange(false)
                         }
-                        .padding(8.dp)
+                        .padding(dimensionResource(R.dimen.small_padding))
                 )
             }
+        }
+    }
+}
+
+/**
+stars namiesto load image co som prepiskol svoje zrucnosti
+stars tuto = https://www.youtube.com/watch?v=bhlQq5s0WHw
+ **/
+@Composable
+fun RatingBar(
+    modifier: Modifier = Modifier,
+    rating: Int = 0,
+    onRatingChanged: (Int) -> Unit,
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+
+        for (index in 1..10) {
+            Icon(
+                imageVector =
+                if (index <= rating) {
+                    Icons.Rounded.Star
+                } else {
+                    Icons.Rounded.StarOutline
+                },
+                contentDescription = null,
+                modifier = Modifier
+                    .size(35.dp)
+                    .clickable { onRatingChanged(index) }
+            )
         }
     }
 }
@@ -166,7 +252,7 @@ fun AddSearchBar(
 @Preview(showBackground = true)
 @Composable
 fun AddScreenPreview() {
-    PathPulseTheme  {
+    PathPulseTheme {
         //AddScreen()
     }
 }
--- app/src/main/java/com/example/pathpulse/screens/memories/AddViewModel.kt
+++ app/src/main/java/com/example/pathpulse/screens/memories/AddViewModel.kt
@@ -1,118 +1,129 @@
 package com.example.pathpulse.screens.memories
 
 
-
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.pathpulse.data.dataMomories.CountriesRepository
 import com.example.pathpulse.data.dataMomories.CountryEntity
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-
-
-data class AddUiState(
-    val countryDetails: CountryDetails = CountryDetails(),
-    val isEntryValid: Boolean = false,
-    val selectedCountry: CountryEntity? = null,
-    val searchResults: List<CountryEntity> = emptyList(),
-    val searchQuery: String = "",
-    val searchActive: Boolean = false
-)
-
-data class CountryDetails(
-    val id: Int = 0,
-    val name: String = "",
-    val description: String? = null,
-    val updatedAt: Long? = null
-)
-
-// mapovanie UI → DB
-fun CountryDetails.toEntity(): CountryEntity =
-    CountryEntity(
-        id = id,
-        name = name,
-        description = description,
-        updatedAt = updatedAt
-    )
-fun CountryEntity.toDetails(): CountryDetails =
-    CountryDetails(
-        id = id,
-        name = name,
-        description = description,
-        updatedAt = updatedAt
-    )
+import kotlinx.coroutines.flow.stateIn
 
 
 class AddViewModel(private val countriesRepository: CountriesRepository) : ViewModel() {
-//    val searchQuery = MutableStateFlow("")
 
-    private var _uiState = MutableStateFlow(AddUiState())
-    val uiState: StateFlow<AddUiState> = _uiState.asStateFlow() //read only
+    private val _uiState = MutableStateFlow(AddUiState())
 
-    init {
-        viewModelScope.launch {
-            uiState
-                .map { it.searchQuery }
-                .debounce(300)
-                .distinctUntilChanged()
-                .flatMapLatest { query ->
-                    if (query.isBlank()) {
-                        countriesRepository.readAll()
-                    } else {
-                        countriesRepository.searchCountries(query)
-                    }
-                }
-                .collect { filtered ->
-                    _uiState.update { it.copy(searchResults = filtered) }
-                }
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    private val searchResults: StateFlow<List<CountryEntity>> =
+        _uiState
+            .map { it.searchQuery }
+            .debounce(300)
+            .distinctUntilChanged()
+            .flatMapLatest { countriesRepository.searchCountries(it) }
+            .stateIn(
+                viewModelScope,
+                SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                emptyList()
+            )
+
+    val uiState: StateFlow<AddUiState> =
+        combine(
+            _uiState,
+            searchResults
+        ) { uistate, results ->
+            uistate.copy(searchResults = results)
         }
+            .stateIn(
+                viewModelScope,
+                SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                AddUiState()
+            )
+
+    fun onRatingChange(newRating: Int) {
+        val old = _uiState.value
+        val details = CountryDetails(
+            id = old.countryDetails.id,
+            name = old.countryDetails.name,
+            description = old.countryDetails.description,
+            updatedAt = old.countryDetails.updatedAt,
+            rating = newRating
+        )
+        updateUiState(details, old)
     }
 
     fun onSearchQueryChange(newQuery: String) {
-        _uiState.update { it.copy(searchQuery = newQuery) }
+        val old = _uiState.value
+        _uiState.value = AddUiState(
+            countryDetails = old.countryDetails,
+            isEntryValid = old.isEntryValid,
+            selectedCountry = old.selectedCountry,
+            searchResults = old.searchResults,
+            searchQuery = newQuery,
+            searchActive = old.searchActive
+        )
     }
 
-    fun onSearchBarActiveChange(active:Boolean) {
-        _uiState.update { it.copy(searchActive = active) }
+    fun onSearchBarActiveChange(active: Boolean) {
+        val old = _uiState.value
+        _uiState.value = AddUiState(
+            countryDetails = old.countryDetails,
+            isEntryValid = old.isEntryValid,
+            selectedCountry = old.selectedCountry,
+            searchResults = old.searchResults,
+            searchQuery = old.searchQuery,
+            searchActive = active
+        )
     }
 
-    fun onSearchQuerySubmit() {
-
-    }
+//    fun onSearchQuerySubmit() {
+//
+//    }
 
     fun onDescriptionChange(newDescription: String) {
-        val updatedDetails = _uiState.value.countryDetails.copy(description = newDescription)
-        _uiState.update {
-            it.copy(
-                countryDetails = updatedDetails,
-                isEntryValid = validateInput(updatedDetails)
-            )
-        }
+        val old = _uiState.value
+        val details = CountryDetails(
+            id = old.countryDetails.id,
+            name = old.countryDetails.name,
+            description = newDescription,
+            updatedAt = old.countryDetails.updatedAt,
+            rating = old.countryDetails.rating
+        )
+        updateUiState(details, old)
     }
 
     fun onCountrySelected(country: CountryEntity) {
-        val details = country.toDetails()
-        _uiState.update {
-            it.copy(
-                selectedCountry = country,
-                countryDetails = details,
-                isEntryValid = validateInput(details),
-                searchQuery = country.name
-            )
-        }
-//        searchQuery.value = country.name
+        val old = _uiState.value
+        val fromDbCountry = country.toDetails()
+        val details = CountryDetails(
+            id = fromDbCountry.id,
+            name = fromDbCountry.name,
+            description = old.countryDetails.description,
+            updatedAt = old.countryDetails.updatedAt,
+            rating = old.countryDetails.rating
+        )
+
+        _uiState.value = AddUiState(
+            countryDetails = details,
+            isEntryValid = validateInput(details),
+            selectedCountry = country,
+            searchResults = old.searchResults,
+            searchQuery = old.searchQuery,
+            searchActive = old.searchActive
+        )
     }
 
     private fun validateInput(details: CountryDetails): Boolean =
-        details.name.isNotBlank() && !details.description.isNullOrBlank()
+        details.name.isNotBlank() && details.description.isNotBlank() && details.rating > 0
 
     suspend fun save() {
         val state = _uiState.value
@@ -121,4 +132,51 @@ class AddViewModel(private val countriesRepository: CountriesRepository) : ViewM
             countriesRepository.updateDescriptionByName(updatedEntity)
         }
     }
+
+    private fun updateUiState(details: CountryDetails, old: AddUiState) {
+        _uiState.value = AddUiState(
+            countryDetails = details,
+            isEntryValid = validateInput(details),
+            selectedCountry = old.selectedCountry,
+            searchResults = old.searchResults,
+            searchQuery = old.searchQuery,
+            searchActive = old.searchActive
+        )
+    }
 }
+
+data class AddUiState(
+    val countryDetails: CountryDetails = CountryDetails(),
+    val isEntryValid: Boolean = false,
+    val selectedCountry: CountryEntity? = null,
+    val searchResults: List<CountryEntity> = emptyList(),
+    val searchQuery: String = "",
+    val searchActive: Boolean = false,
+)
+
+data class CountryDetails(
+    val id: Int = 0,
+    val name: String = "",
+    val description: String = "",
+    val updatedAt: Long = 0L,
+    val rating: Int = 0
+)
+
+fun CountryDetails.toEntity(): CountryEntity =
+    CountryEntity(
+        id = id,
+        name = name,
+        description = description,
+        updatedAt = updatedAt,
+        rating = rating
+    )
+
+fun CountryEntity.toDetails(): CountryDetails =
+    CountryDetails(
+        id = id,
+        name = name,
+        description = description.orEmpty(),
+        updatedAt = updatedAt ?: 0L,
+        rating = rating ?: 0
+    )
+
